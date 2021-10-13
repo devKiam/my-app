69b191816644e94a1f7f51bcb52b0baff31e8c38
nerdyKiam <45430833+nerdyKiam@users.noreply.github.com>
2021-10-13 15:02:55 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // array of objects
+    let players = [
+        {name: "Shakib al Hasan", type: "All-rounder"},
+        {name: "Tamim Iqbal", type: "Opener batsman"},
+        {name: "Naim Sheikh", type: "Opener batsman"}
+    ]
+
+    return (
+        <div className="App">
+            {
+                players.map(player =>
+                    <Person name={player.name} type={player.type}/>
+                )
+            }
+            <Counter/>
+            <API/>
+        </div>
+    );
 }
+
+
+// Person component
+function Person(props) {
+    return (
+        <div className="person-class">
+            <h1>{props.name}</h1>
+            <p>{props.type}</p>
+        </div>
+    );
+}
+
+
+// state component example
+function Counter() {
+
+    let [count, setCount] = useState(0)
+
+    //functions for handling button events + -
+    let handleIncrease = () => {
+        setCount(++count)
+    }
+    let handleDecrease = () => {
+        setCount(--count)
+    }
+
+    // do not call the onClick action function
+    return (
+        <div>
+            <h1>Count : {count}</h1>
+            <button onClick={handleIncrease}>+</button>
+            <button onClick={handleDecrease}>-</button>
+        </div>
+    )
+}
+
+
+//component for API users
+function API() {
+
+    let [users, setUsers] = useState([])
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(response => response.json())
+            .then(data => setUsers(data))
+    }, [])
+
+    return (
+        <div>
+            {
+                users.map(user => <h3>{user.name}</h3>)
+            }
+        </div>
+    )
+}
+
 
 export default App;
